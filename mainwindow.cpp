8dfc5efa38cbc504e16825f84bbb2b1cc760e36e
tangchen <tangchen099@qq.com>
2018-05-12 11:42:20 +0800

    impliments functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,32 +1,94 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <string.h>
 #include <QtCore>
 #include <QtGui>
 #include <QDesktopWidget>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QInputDialog>
 
+inline void myIntToArray(int number,char * array)
+{
+    int num;
+    int j = 0;
+    int i = 1;
+    num = number;
+    while (num > 10) {
+        i *= 10;
+        num /= 10;
+    }
+    while (number > 10) {
+        *(array + j++) = number / i + '0';
+        number %= i;
+        i /= 10;
+    }
+    *(array + j++) = number + '0';
+    *(array + j)   = '\0';
+}
 
 MainWindow::MainWindow(QWidget *parent, int argc, char **argv) :
     QWidget(parent),
-    ui(new Ui::MainWindow)
+    mainWindow(new Ui::MainWindow),
+    informationWindow(NULL)
 {
-    ui->setupUi(this);
+    mainWindow->setupUi(this);
 
     objectDetector = new ObjectDetect();
     if (!objectDetector->readParameters(argc,argv)) {
         printf("some parameters read failed,now use default\n");
     }
 
+    QString path = QString(objectDetector->getCascadeName());
+    mainWindow->cascadeClassfierLabel->setText(tr("Classfiler:\n") + path);
+
+    if (objectDetector->isFromCamera()) {
+        char number[4];
+        myIntToArray(objectDetector->getCameraDevice(),number);
+        QString idx = QString(number);
+        mainWindow->fileLable->setText(tr("Camera Device : ") + idx);
+        mainWindow->openCameraButton->setText(tr("Change"));
+        mainWindow->openVideoButton->setEnabled(false);
+        mainWindow->openDirectoryButton->setEnabled(false);
+    } else if (objectDetector->isFromFile()) {
+        QString path = QString(objectDetector->getFileDirectory());
+        mainWindow->fileLable->setText(tr("Directory:\n") + path);
+        mainWindow->openDirectoryButton->setText(tr("Change"));
+        mainWindow->openCameraButton->setEnabled(false);
+        mainWindow->openVideoButton->setEnabled(false);
+    } else if (objectDetector->isFromVideo()) {
+        QString path = QString(objectDetector->getVideoname());
+        mainWindow->fileLable->setText(tr("Video Name:\n") + path);
+        mainWindow->openCameraButton->setEnabled(false);
+        mainWindow->openDirectoryButton->setEnabled(false);
+    }
+
     QDesktopWidget* desktop = QApplication::desktop();
     //attempt to center MainWindow widget
     this->move((desktop->width() - this->width())/2, (desktop->height() - this->height())/2);
+    timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(on_timeout()));
+}
+
+void MainWindow::closeEvent(QCloseEvent *)
+{
+    if (informationWindow != NULL) {
+        informationWindow->close();
+    }
+    printf("\nclose\n");
+}
+
+//solt of time out
+void MainWindow::changeEvent(QEvent *)
+{
+
 }
 
 MainWindow::~MainWindow()
 {
     delete objectDetector;
-    delete ui;
+    delete mainWindow;
+    if (informationWindow != NULL) delete informationWindow;
 }
 
 void MainWindow::on_openClassfilerButton_clicked()
@@ -38,47 +100,130 @@ void MainWindow::on_openClassfilerButton_clicked()
     if(fileDialog->exec() == QDialog::Accepted) {
         QString path = fileDialog->selectedFiles()[0];
         if (objectDetector->setObjectCascadeName(path.toStdString().c_str())) {
-            ui->cascadeClassfierLabel->setText(tr("Classfiler:\n") + path);
+            mainWindow->cascadeClassfierLabel->setText(tr("Classfiler:\n") + path);
         } else {
-            ui->cascadeClassfierLabel->setText(tr("Cascade Classfiler not loaded"));
+            mainWindow->cascadeClassfierLabel->setText(tr("Cascade Classfiler not loaded"));
         }
     }
 }
 
 void MainWindow::on_openDirectoryButton_clicked()
 {
-    QFileDialog *fileDialog = new QFileDialog(this);
-    fileDialog->setWindowTitle(tr("Select Directory"));
-    fileDialog->setDirectory(".");
-    fileDialog->setFilter(QDir::Dirs);
-    fileDialog->setFileMode(QFileDialog::Directory);
-    if(fileDialog->exec() == QDialog::Accepted) {
-        QString path = fileDialog->selectedFiles()[0];
-        if (objectDetector->setFileDirectory(path.toStdString().c_str())) {
-            ui->fileLable->setText(tr("Directory:\n") + path);
-        } else {
-            ui->cascadeClassfierLabel->setText(tr("File not loaded"));
+    if (mainWindow->openDirectoryButton->text() == (QString)"Change") {
+        objectDetector->setFromFile(false);
+        mainWindow->openDirectoryButton->setText(tr("OpenDirectory"));
+        mainWindow->openCameraButton->setEnabled(true);
+        mainWindow->openVideoButton->setEnabled(true);
+        mainWindow->fileLable->setText(tr("File not loaded"));
+    } else {
+        QFileDialog *fileDialog = new QFileDialog(this);
+        fileDialog->setWindowTitle(tr("Select Directory"));
+        fileDialog->setDirectory(".");
+        fileDialog->setFilter(QDir::Dirs);
+        fileDialog->setFileMode(QFileDialog::Directory);
+        if(fileDialog->exec() == QDialog::Accepted) {
+            QString path = fileDialog->selectedFiles()[0];
+            if (objectDetector->setFileDirectory(path.toStdString().c_str())) {
+                objectDetector->setFromFile(true);
+                mainWindow->fileLable->setText(tr("Directory:\n") + path);
+                mainWindow->openDirectoryButton->setText(tr("Change"));
+                mainWindow->openCameraButton->setEnabled(false);
+                mainWindow->openVideoButton->setEnabled(false);
+            } else {
+                mainWindow->fileLable->setText(tr("File not loaded"));
+            }
         }
     }
 }
 
 void MainWindow::on_openVideoButton_clicked()
 {
-    QFileDialog *fileDialog = new QFileDialog(this);
-    fileDialog->setWindowTitle(tr("Select Video"));
-    fileDialog->setDirectory(".");
-    fileDialog->setNameFilter(tr("Video Files(*.mp4 *.avi *.rmvb)"));
-    if(fileDialog->exec() == QDialog::Accepted) {
-        QString path = fileDialog->selectedFiles()[0];
-        if (objectDetector->setVideoname(path.toStdString().c_str())) {
-            ui->fileLable->setText(tr("Video Name:\n") + path);
-        } else {
-            ui->cascadeClassfierLabel->setText(tr("File not loaded"));
+    if (mainWindow->openVideoButton->text() == (QString)"Change") {
+        objectDetector->setFromVideo(false);
+        mainWindow->openVideoButton->setText(tr("OpenVideo"));
+        mainWindow->openCameraButton->setEnabled(true);
+        mainWindow->openDirectoryButton->setEnabled(true);
+        mainWindow->fileLable->setText(tr("File not loaded"));
+    } else {
+        QFileDialog *fileDialog = new QFileDialog(this);
+        fileDialog->setWindowTitle(tr("Select Video"));
+        fileDialog->setDirectory(".");
+        fileDialog->setNameFilter(tr("Video Files(*.mp4 *.avi *.rmvb)"));
+        if(fileDialog->exec() == QDialog::Accepted) {
+            QString path = fileDialog->selectedFiles()[0];
+            if (objectDetector->setVideoname(path.toStdString().c_str())) {
+                objectDetector->setFromVideo(true);
+                mainWindow->fileLable->setText(tr("Video Name:\n") + path);
+                mainWindow->openVideoButton->setText(tr("Change"));
+                mainWindow->openCameraButton->setEnabled(false);
+                mainWindow->openDirectoryButton->setEnabled(false);
+            } else {
+                mainWindow->fileLable->setText(tr("File not loaded"));
+            }
         }
     }
 }
 
 void MainWindow::on_openCameraButton_clicked()
 {
+    if (mainWindow->openCameraButton->text() == (QString)"Change") {
+        objectDetector->setFromCamera(false);
+        mainWindow->openCameraButton->setText(tr("OpenCamera"));
+        mainWindow->openVideoButton->setEnabled(true);
+        mainWindow->openDirectoryButton->setEnabled(true);
+        mainWindow->fileLable->setText(tr("File not loaded"));
+    } else {
+        bool result;
+        int index = QInputDialog::getInt(this,tr("Input Index"),tr("Please input index"),0,0,99,1,&result);
+        if (result) {
+            char number[4];
+            myIntToArray(index,number);
+            QString idx = QString(number);
+            objectDetector->setCameraDevice(index);
+            objectDetector->setFromCamera(true);
+            mainWindow->fileLable->setText(tr("Camera Device : ") + idx);
+            mainWindow->openCameraButton->setText(tr("Change"));
+            mainWindow->openVideoButton->setEnabled(false);
+            mainWindow->openDirectoryButton->setEnabled(false);
+        } else {
+            mainWindow->fileLable->setText(tr("File not loaded"));
+        }
+    }
+}
 
+void MainWindow::on_detectButton_clicked()
+{
+    objectDetector->init();
+    if (informationWindow == NULL) {
+        informationWindow = new InformationWindow();
+        informationWindow->show();
+    } else {
+        delete informationWindow;
+        informationWindow = new InformationWindow();
+        informationWindow->show();
+    }
+    timer->start(1);
+}
+
+void MainWindow::on_timeout()
+{
+    cv::Mat frame;
+    frame = objectDetector->loadFrame(1);
+    // start time
+    const clock_t begin_time = clock();
+    objectDetector->detectObjects(frame);
+    // end time
+    float seconds = float(clock() - begin_time) / CLOCKS_PER_SEC;
+    //colorful print
+    fprintf(stdout," time cost: \033[0;32m%.3f\033[0m s %d\r", seconds,frame.channels());
+    fflush(stdout);
+    for (size_t i = 0; i < objectDetector->objects.size(); i++) {
+        //draw rectangle
+        rectangle(frame, objectDetector->objects[i], cv::Scalar(0, 0, 255));
+    }
+    if (frame.channels() == 3) {
+        cv::cvtColor(frame,frame,cv::COLOR_BGR2RGB);
+    }
+    QImage qimage = QImage(static_cast<uchar *>(frame.data),frame.cols,frame.rows,QImage::Format_RGB888);
+    mainWindow->imageLabel->setPixmap(QPixmap::fromImage(qimage));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,10 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 #include "objectdetect.h"
+#include "informationwindow.h"
 #include <QWidget>
+#include <QCloseEvent>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -15,6 +18,10 @@ public:
     explicit MainWindow(QWidget *parent = 0, int argc = 0, char **argv = NULL);
     ~MainWindow();
 
+protected:
+    void closeEvent(QCloseEvent *event);
+    void changeEvent(QEvent *event);
+
 private slots:
     void on_openClassfilerButton_clicked();
 
@@ -24,9 +31,15 @@ private slots:
 
     void on_openCameraButton_clicked();
 
+    void on_detectButton_clicked();
+
+    void on_timeout();
+
 private:
-    Ui::MainWindow *ui;
-    ObjectDetect * objectDetector;
+    Ui::MainWindow *mainWindow;
+    InformationWindow *informationWindow;
+    ObjectDetect *objectDetector;
+    QTimer *timer;
 };
 
 #endif // MAINWINDOW_H
